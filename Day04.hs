de771be922ee9946834fce528eb21f982576f1c9
angusjf <angus.findlay@prima.it>
2023-12-04 10:43:33 +0000

    day04

---
+++ Day04.hs
@@ -0,0 +1,25 @@
+import Data.List
+import Data.Array
+import Data.Char
+import Debug.Trace
+
+test = readFile "test04.txt" >>= print . two
+main = readFile "input04.txt" >>= print . two
+
+two =
+    sum .
+    map fst .
+    f .
+    map (\x -> (1, x)) .
+    map length . 
+    map (\(winners, have) -> intersect winners have) .
+    map parse .
+    lines
+
+parse :: String -> ([Int], [Int])
+parse ('C':'a':'r':'d':' ':more) = (map read $ words winners, map read $ words have)
+  where (n, ':':numbers) = break (== ':') more
+        (winners, '|':have) = break (== '|') numbers
+
+f ((n, x):more) = (n, x) : f ( (map (\(nn, xx) -> (nn + n, xx)) (take x more)) ++ (drop x more) )
+f [] = []
