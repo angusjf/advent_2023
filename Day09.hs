8566773df6ec3d5dd8b7a6dfbad4ddfe16c90071
angusjf <angus.findlay@prima.it>
2023-12-09 09:50:54 +0000

    cleanup 09

--- Day09.hs
+++ Day09.hs
@@ -1,19 +1,13 @@
-import Data.List
-import qualified Data.Map as M
-import Data.Map ((!))
-import Data.Char
-import Debug.Trace
-
 test = pt2 <$> readFile "test09.txt"
 main = pt2 <$> readFile "input09.txt"
 
 pt2 =
-  -- sum . 
+  sum .
   map (solve . map read . words) .
   lines
 
-solve xs = foldr1 (-) $ map head $ triangulate xs
+solve = foldr1 (-) . map head . triangulate
 
-triangulate xs = takeWhile (not . all (== 0)) $ iterate diffs xs
+triangulate = takeWhile (not . all (== 0)) . iterate diffs
 
 diffs xs = zipWith (flip (-)) xs (tail xs)
