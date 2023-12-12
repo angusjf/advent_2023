b327c8cf6fb224f1c8594bbfc8c33f95e4ef09f2
angusjf <angus.findlay@prima.it>
2023-12-12 22:36:17 +0000

    day12 part 2 w/ cache

--- Day12.hs
+++ Day12.hs
@@ -1,15 +1,15 @@
-import Data.Array
 import Data.List
 import Data.Maybe
 import Data.Tuple
+import qualified Data.Map as M
 import Debug.Trace
 
 test = pt2 <$> readFile "test12.txt"
 main = pt2 <$> readFile "input12.txt"
 
-pt1 input = map (uncurry solve . parse) $ lines input
+-- pt1 input = map (uncurry solve . parse) $ lines input
 
-pt2 input = map (uncurry solve . scale . parse) $ lines input
+pt2 input = map (fst . uncurry (solveCache M.empty) . scale . parse) $ lines input
 
 scale (springs, breaks) = (concat $ intersperse "?" $ take 5 $ repeat springs, concat $ take 5 $ repeat breaks)
 
@@ -26,18 +26,34 @@ split f s =
         ( a,  []) -> [a]
         ( a, _:b) -> [a] ++ split f b
 
-solve "" [] = 1
-solve "" _  = 0
+type Cache = M.Map (String, [Int]) Int
 
-solve springs [] = if '#' `elem` springs then 0 else 1
+solveCache  :: Cache -> String -> [Int] -> (Int, Cache)
+solveCache dict springs breaks =
+    case M.lookup (springs, breaks) dict of
+	Just x -> (x, dict)
+	Nothing ->
+	    let
+	      (res, dict') = solve dict springs breaks
+	      dict'' = M.insert (springs, breaks) res dict'
+	    in
+	      (res, dict'')
 
-solve ('.':springs) breaks = solve springs breaks
+solve :: Cache -> String -> [Int] -> (Int, Cache)
+solve c "" [] = (1, c)
+solve c "" _  = (0, c)
 
-solve ('?':springs) breaks = solve ('.':springs) breaks + solve ('#':springs) breaks
+solve c springs [] = (if '#' `elem` springs then 0 else 1, c)
 
-solve ('#':sprongs) (b:breaks) =
-        if b <= length sprongs + 1
-	&& not ('.' `elem` take (b - 1) sprongs)
-	&& (b == length sprongs + 1 || sprongs !! (b - 1) /= '#')
-	  then solve (drop b sprongs) breaks
-          else 0
+solve c ('.':springs) breaks = solveCache c springs breaks
+
+solve c ('?':springs) breaks = (x + y, dict'')
+  where (x, dict') = solveCache c ('.':springs) breaks
+        (y, dict'') = solveCache dict' ('#':springs) breaks
+
+solve c ('#':springs) (b:breaks) =
+        if b <= length springs + 1
+	&& not ('.' `elem` take (b - 1) springs)
+	&& (b == length springs + 1 || springs !! (b - 1) /= '#')
+	  then solveCache c (drop b springs) breaks
+          else (0, c)
