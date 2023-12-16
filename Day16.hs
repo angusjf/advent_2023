502b49d6fcfc50afcc1f6aef33bb694a03ec5c8a
angusjf <angus.findlay@prima.it>
2023-12-16 19:11:30 +0000

    Day16 - MArray

--- Day16.hs
+++ Day16.hs
@@ -1,47 +1,62 @@
 import Data.List
-import Data.Array
+import Data.Array.MArray
+import Data.Array.IO
 import Data.Complex
+import Data.Foldable
 
-test = pt2 <$> readFile "test16.txt"
-main = pt2 <$> readFile "input16.txt"
+test1 = readFile "test16.txt" >>= pt1
+main1 = readFile "input16.txt" >>= pt1
 
-to2dArray ls = listArray ((0, 0), (length (head ls) - 1, length ls - 1)) (concat $ transpose $ ls)
+test = readFile "test16.txt" >>= pt2
+main = readFile "input16.txt" >>= pt2 >>= putStrLn . unlines . map show
 
-pt1 =
-  unvisted .
-  solve (0, 0) (1 :+ 0) .
-  fmap (\c -> (c, [])) .
-  to2dArray .
-  lines
+to2dArray :: [String] -> IO (IOArray (Int, Int) Char)
+to2dArray ls = newListArray ((0, 0), (length (head ls) - 1, length ls - 1)) (concat $ transpose $ ls)
 
-pt2 s =
-  let
-    grid = fmap (\c -> (c, [])) $ to2dArray $ lines $ s
-  in
-    maximum [ unvisted $ solve pos dir grid
+pt1 :: String -> IO Int
+pt1 s = do
+  a <- to2dArray (lines s)
+  a' <- mapArray (\c -> (c, [])) a
+  solved <- solve (0, 0) (1 :+ 0) a'
+  unvisted solved
+
+copy :: IOArray (Int, Int) (Char, [Complex Float]) -> IO (IOArray (Int, Int) (Char, [Complex Float]))
+copy = mapArray id
+
+pt2 s = do
+   a <- to2dArray (lines s)
+   bounds <- getBounds a
+   a' <- mapArray (\c -> (c, [])) a
+   sequence [ copy a' >>= solve pos dir >>= unvisted
             | dir <- [0 :+ 1, 0 :+ (-1), 1 :+ 0, (-1) :+ 0] 
-            , pos <- borders grid
+            , pos <- borders bounds
             ]
 
-unvisted = length . filter (not . null . snd) . elems
+unvisted :: (IOArray (Int, Int) (Char, [Complex Float])) -> IO Int
+unvisted a = do
+               es <- getElems a
+               return $ length $ filter (not . null . snd) es
 
-solve (x, y) _ grid | x < 0 || y < 0 || x > maxX || y > maxY = grid
-  where ((0, 0), (maxX, maxY)) = bounds grid
-
-solve pos dir oldGrid =
-  let (c, visited) = oldGrid ! pos
-      grid = oldGrid // [(pos, (c, dir:visited))]
-      dirs =
-        case c of
-          '|' -> if realPart dir /= 0 then [dir * i, dir * (-i)] else [dir]
-          '-' -> if imagPart dir /= 0 then [dir * i, dir * (-i)] else [dir]
-          '/' -> [mirrorXY dir]
-          '\\' -> [mirrorX_Y dir]
-          '.' -> [dir]
-  in
-  if not $ dir `elem` visited then
-    foldr (\d g -> solve (move pos d) d g) grid dirs
-    else oldGrid
+solve ::(Int, Int) -> Complex Float -> IOArray (Int, Int) (Char, [Complex Float]) -> IO ( IOArray (Int, Int) (Char, [Complex Float]) )
+solve pos@(x, y) dir oldGrid =
+  do
+    ((0, 0), (maxX, maxY)) <- getBounds oldGrid
+    if x < 0 || y < 0 || x > maxX || y > maxY then
+      return oldGrid
+    else do
+      (c, visited) <- readArray oldGrid pos 
+      if not $ dir `elem` visited
+        then do
+          () <- writeArray oldGrid pos (c, dir:visited)
+          let dirs = case c of
+                       '|' -> if realPart dir /= 0 then [dir * i, dir * (-i)] else [dir]
+                       '-' -> if imagPart dir /= 0 then [dir * i, dir * (-i)] else [dir]
+                       '/' -> [mirrorXY dir]
+                       '\\' -> [mirrorX_Y dir]
+                       '.' -> [dir]
+          let xs = map (\d -> solve (move pos d) d oldGrid) dirs
+          last <$> sequence xs
+        else return oldGrid
 
 i = 0 :+ 1
 
@@ -51,9 +66,8 @@ mirrorXY (a :+ b) = (-b) :+ (-a)
 
 move (x, y) (a :+ b) = (x + floor a, y + floor b)
 
-borders grid = concat [ [ (0,    y) | y <- [0 .. maxY] ]
+borders ((0, 0), (maxX, maxY)) = concat [ [ (0,    y) | y <- [0 .. maxY] ]
                       , [ (maxX, y) | y <- [0 .. maxY] ]
                       , [ (x,    0) | x <- [0 .. maxX] ]
                       , [ (x, maxY) | x <- [0 .. maxX] ]
                       ]
-  where ((0, 0), (maxX, maxY)) = bounds grid
