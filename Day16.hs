82b90c834576124376e637d5f8dd47e720f812cd
angusjf <angus.findlay@prima.it>
2023-12-16 15:03:33 +0000

    cleanup 16 more

--- Day16.hs
+++ Day16.hs
@@ -1,71 +1,29 @@
 import Data.List
-import Data.Char
 import Data.Array
-import Debug.Trace
+import Data.Complex
 
 test = pt2 <$> readFile "test16.txt"
 main = pt2 <$> readFile "input16.txt"
 
 to2dArray ls = listArray ((0, 0), (length (head ls) - 1, length ls - 1)) (concat $ transpose $ ls)
 
-data Dir = N | E | S | W deriving (Enum, Show, Eq)
-
-r90c W = N
-r90c x = succ x
-
-r90a N = W
-r90a x = pred x
-
-mirrorXY N = E
-mirrorXY E = N
-mirrorXY S = W
-mirrorXY W = S
-
-mirrorX_Y N = W
-mirrorX_Y W = N
-mirrorX_Y S = E
-mirrorX_Y E = S
-
-move (x, y) N = (x, y - 1)
-move (x, y) S = (x, y + 1)
-move (x, y) E = (x + 1, y)
-move (x, y) W = (x - 1, y)
-
-pt1 s =
-  let grid = fmap (\c -> (c, [])) $ to2dArray $ lines $ s
-  in unvisted $ solve (0, 0) E grid
-
-unvisted = length . filter (not . null . snd) . elems
+pt1 =
+  unvisted .
+  solve (0, 0) (1 :+ 0) .
+  fmap (\c -> (c, [])) .
+  to2dArray .
+  lines
 
 pt2 s =
   let
     grid = fmap (\c -> (c, [])) $ to2dArray $ lines $ s
-    ((0, 0), (maxX, maxY)) = bounds grid
   in
-    maximum $
-    map snd $
-    map
-      (\(pos, dir) -> ((pos, dir), unvisted $ solve pos dir grid))
-      ( concat [ [ ((0, y), dir)
-                 | y <- [0 .. maxY]
-                 , dir <- [N, S, E, W]
-                 ]
-               , [ ((maxX, y), dir)
-                 | y <- [0 .. maxY]
-                 , dir <- [N, S, E, W]
-                 ]
-               , [ ((x, 0), dir)
-                 | x <- [0 .. maxX]
-                 , dir <- [N, S, E, W]
-                 ]
-               , [ ((x, maxY), dir)
-                 | x <- [0 .. maxX]
-                 , dir <- [N, S, E, W]
-                 ]
-               ])
-      
+    maximum [ unvisted $ solve pos dir grid
+            | dir <- [0 :+ 1, 0 :+ (-1), 1 :+ 0, (-1) :+ 0] 
+            , pos <- borders grid
+            ]
 
-solve :: (Int, Int) -> Dir -> Array (Int, Int) (Char, [Dir]) -> Array (Int, Int) (Char, [Dir])
+unvisted = length . filter (not . null . snd) . elems
 
 solve (x, y) _ grid | x < 0 || y < 0 || x > maxX || y > maxY = grid
   where ((0, 0), (maxX, maxY)) = bounds grid
@@ -75,8 +33,8 @@ solve pos dir oldGrid =
       grid = oldGrid // [(pos, (c, dir:visited))]
       dirs =
         case c of
-          '|' -> if dir `elem` [E, W] then [r90c dir, r90a dir] else [dir]
-          '-' -> if dir `elem` [N, S] then [r90c dir, r90a dir] else [dir]
+          '|' -> if realPart dir /= 0 then [dir * i, dir * (-i)] else [dir]
+          '-' -> if imagPart dir /= 0 then [dir * i, dir * (-i)] else [dir]
           '/' -> [mirrorXY dir]
           '\\' -> [mirrorX_Y dir]
           '.' -> [dir]
@@ -84,3 +42,18 @@ solve pos dir oldGrid =
   if not $ dir `elem` visited then
     foldr (\d g -> solve (move pos d) d g) grid dirs
     else oldGrid
+
+i = 0 :+ 1
+
+mirrorX_Y (a :+ b) = b :+ a
+
+mirrorXY (a :+ b) = (-b) :+ (-a)
+
+move (x, y) (a :+ b) = (x + floor a, y + floor b)
+
+borders grid = concat [ [ (0,    y) | y <- [0 .. maxY] ]
+                      , [ (maxX, y) | y <- [0 .. maxY] ]
+                      , [ (x,    0) | x <- [0 .. maxX] ]
+                      , [ (x, maxY) | x <- [0 .. maxX] ]
+                      ]
+  where ((0, 0), (maxX, maxY)) = bounds grid
